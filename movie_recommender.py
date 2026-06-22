c3e9160aa85cd3d6d1b0328559ece1d4c35f9191
AjayVish232 <ajaysingh1aa1@gmail.com>
2026-06-22 15:13:41 +0530

    Add recommendation engine

---
+++ movie_recommender.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+movies = pd.read_csv('dataset/movies.csv')
+
+cv = CountVectorizer(max_features=5000, stop_words='english')
+vectors = cv.fit_transform(movies['tags']).toarray()
+
+similarity = cosine_similarity(vectors)
+
+def recommend(movie):
+    movie_index = movies[movies['title'] == movie].index[0]
+    distances = similarity[movie_index]
+    movie_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
+
+    for i in movie_list:
+        print(movies.iloc[i[0]].title)
